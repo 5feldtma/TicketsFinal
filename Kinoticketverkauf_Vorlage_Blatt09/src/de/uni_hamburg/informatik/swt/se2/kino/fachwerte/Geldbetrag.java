2f821b4ef64a7def1acd44d7cc3bde415305ff8b
Simon <Simon@10.80.171.1>
2016-07-11 01:15:55 +0200

    Alt. Lösungsvorschlag "10" -> 1000 Cent

--- Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
+++ Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
@@ -8,7 +8,7 @@ public final class Geldbetrag implements Comparable<Geldbetrag>
     
     private final int _centBetrag;
     
-    private final static Pattern _pattern = Pattern.compile("(\\d{0,})(,?)(\\d{1,2})");
+    private final static Pattern _pattern = Pattern.compile("(\\d{0,})(,?)(\\d{0,2})");
     
 
     private Geldbetrag(int betrag)
@@ -21,6 +21,8 @@ public final class Geldbetrag implements Comparable<Geldbetrag>
         Matcher matcher = _pattern.matcher(s);
         int betrag = 0;
         matcher.matches();
+        
+        
         if(matcher.group(2).equals(","))
         {
             betrag = (int) (Integer.parseInt(matcher.group(3))*(100/Math.pow(10, matcher.group(3).length())));
@@ -33,6 +35,18 @@ public final class Geldbetrag implements Comparable<Geldbetrag>
             betrag = Integer.parseInt(matcher.group(0));
             System.out.println("a: " + betrag);
         }
+        
+        /*
+        if(matcher.group(2).equals(","))
+        {
+            betrag = (int) (Integer.parseInt(matcher.group(3))*(100/Math.pow(10, matcher.group(3).length())));
+            System.out.println("b: " + betrag);
+        }
+            
+        betrag += Integer.parseInt(matcher.group(1))*100;
+        System.out.println("c: " + betrag);
+        */
+        
         return new Geldbetrag(betrag);
     }
     
